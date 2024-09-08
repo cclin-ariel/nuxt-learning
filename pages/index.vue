072ffe25d39a1a61997f911f34ca9db944fdbb09
cclin <71698978+cclin-ariel@users.noreply.github.com>
2024-09-08 11:34:26 +0900

    初始提交

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { useFetch } from "nuxt/app";
+
+const { data } = await useFetch("https://api.example.com/data");
+</script>
+
+<template>
+  <h1>Hello world!</h1>
+</template>
